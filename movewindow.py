b907a177b7e3769f4c8b2e6df898ef2ecf6dd574
korayzorlu <89906311+korayzorlu@users.noreply.github.com>
2021-09-19 15:15:23 +0300

    Update movewindow.py

--- movewindow.py
+++ movewindow.py
@@ -2,14 +2,14 @@ def moveWindow(e):
     global clickPosition
     if mainWindow.isMaximized() == False:
         if e.buttons() == Qt.LeftButton:
-            mainWindow.move(mainWindow.pos() + e.globalPos() - clickPosition)
+            mainWindow.move(mainWindow.pos() + e.globalPos() - clickPosition) #move window
             clickPosition = e.globalPos()
             e.accept()
 
-label1.mouseMoveEvent = moveWindow
+label1.mouseMoveEvent = moveWindow #move label
 
 def mousePressEvent(event):
     global clickPosition
     clickPosition = event.globalPos()
 
-label1.mousePressEvent = mousePressEvent
+label1.mousePressEvent = mousePressEvent #label clicked signal
